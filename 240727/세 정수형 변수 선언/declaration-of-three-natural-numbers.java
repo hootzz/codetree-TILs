afec2da5ebe37f9248881b1ca9b9bbacd58fb3ba
hootzz <128481582+hootzz@users.noreply.github.com>
2024-07-27 00:51:27 +0900

    Title: 세 정수형 변수 선언, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240727/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        // 변수 포맷 %d(수), %s(string) 
+        // System.out.prinf() 사용
+        // \n 은 한 줄 띄어쓰기
+        int a = 7;
+        int b = 23;
+        System.out.printf("%d + %d = %d", a, b, (a+b));
+    }
+}
