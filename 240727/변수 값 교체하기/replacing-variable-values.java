0182d95d5771412aa4b5e47d389f21e565e0f14a
hootzz <128481582+hootzz@users.noreply.github.com>
2024-07-27 19:53:26 +0900

    Title: 변수 값 교체하기, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240727/변수 값 교체하기/replacing-variable-values.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 3;
+        // 변수 값은 바꿀 수 있음
+        a = 6;
+        System.out.print(a);
+    }
+}
