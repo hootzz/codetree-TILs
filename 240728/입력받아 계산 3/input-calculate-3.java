f95fa8c5ee4b2bd7de81956893c38c0bc5b6e80d
hootzz <128481582+hootzz@users.noreply.github.com>
2024-07-28 16:33:54 +0900

    Title: 입력받아 계산 3, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240728/입력받아 계산 3/input-calculate-3.java
+++ 240728/입력받아 계산 3/input-calculate-3.java
@@ -6,6 +6,6 @@ public class Main {
         // 2개의 줄에 걸력서 입력할 때도 공백의 경우와 동일
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.println(a+b);
+        System.out.println(a*b);
     }
 }
