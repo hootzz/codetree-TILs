e4d7cb31193d1314501a78fa04cd3dca97643021
hootzz <128481582+hootzz@users.noreply.github.com>
2024-07-29 17:35:52 +0900

    Title: 합과 평균, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240729/합과 평균/sum-and-avg.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+        // 캐스팅은 (double), (int)를 앞에 붙이면 된다
+        // 
+        System.out.println
+    }
+}
