dc5af03217b8b237633e289b96193a98644734bd
hootzz <128481582+hootzz@users.noreply.github.com>
2024-07-29 20:13:29 +0900

    Title: 삼항연산자, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240729/삼항연산자/ternary-operator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        String result;
+        
+        // 등급 부여 제외하고 100 점만 따졌을 때
+        //a = sc.nextInt();
+        //result = a == 100 ? "pass" : "failure";
+        //System.out.println(result);
+
+        // 점수에 따른 등급 부여 코드도 작성해 본다면?
+        int x;
+        x = -((a / 10) - 10);
+        result = x == 0 ? "pass" : "failure";
+        System.out.println(result);
+    }
+}
