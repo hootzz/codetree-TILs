e12c15888a1a95b33fe84c08d5052982699309e6
hootzz <128481582+hootzz@users.noreply.github.com>
2024-07-29 20:51:26 +0900

    Title: 살 수 있는 물건, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240729/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String result;
+
+        if (n >= 3000) {
+            result = "book";
+        } else if (n >= 1000) {
+            result = "mask";
+        } else {
+            result = "no";
+        }
+
+        System.out.println(result);
+    }
+}
