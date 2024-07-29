cb14bb90a7798137ea6153008dc505a579230d11
hootzz <128481582+hootzz@users.noreply.github.com>
2024-07-29 21:55:47 +0900

    Title: 4가지 관계연산자, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240729/4가지 관계연산자/four-relational-operators.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+        int result;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        if (a >= b) {
+            result = 1;
+        } else {
+            result = 0;
+        }
+        System.out.println(result);
+
+        if (a > b) {
+            result = 1;
+        } else {
+            result = 0;
+        }
+        System.out.println(result);
+
+        if (b >= a) {
+            result = 1;
+        } else {
+            result = 0;
+        }
+        System.out.println(result);
+
+        if (b > a) {
+            result = 1;
+        } else {
+            result = 0;
+        }
+        System.out.println(result);
+    }
+}
